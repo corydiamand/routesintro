f6b747c85914ded4ac63d9da4ac6451edf75ec0e
Cory Diamand <cory@postpourri.com>
2014-02-21 02:35:49 -0500

    created a super routes file for teaching, also scaffolded up a cats resource

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Routesintro::Application.config.secret_key_base = '754eaaedea267aee05309699dbaf44dcf54412c5f117bf0ffb9bfbaee30bc36474d68b79f8ecaf883a9b3253f74349081b8dcbd57d1f4b5afdccf89914e93223'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Routesintro::Application.config.session_store :cookie_store, key: '_routesintro_session'
